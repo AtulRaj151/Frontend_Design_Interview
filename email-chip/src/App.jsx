c9d91f54ba0b767248ca4dd89e1dcc431aba695f
AtulRaj151 <atulsid151@gmail.com>
2024-02-29 12:09:14 +0530

    added chip: in below container

---
+++ email-chip/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import './App.css'
+
+const isValidEmail = (mail) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(mail);
+}
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [emailList, setEmailList] = useState([]);
+
+
+  const handleAddBtn = () => {
+    let mail = email;
+    if (mail.trim() !== '') {
+      if (isValidEmail(mail)) {
+        setEmailList([...emailList, { email: mail, isValid: true }]);
+      } else {
+        setEmailList([...emailList, { email: mail, isValid: false }]);
+      }
+      setEmail('');
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+
+  const handleOnKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleAddBtn();
+    }
+  }
+
+  const getAvatar = (email) => {
+    return `https://i.pravatar.cc/150?u=${email}`;
+  };
+
+  const handleDelete = (index) => {
+    setEmailList((prev) => {
+      const updated = [...prev];
+      updated.splice(index, 1);
+      return updated;
+    })
+  }
+
+  return (
+    <>
+      <div id='container'>
+        <div className='input-wrapper'>
+          <input className='email-input' type='text' onChange={handleEmailChange} onKeyDown={handleOnKeyPress} value={email} />
+          <button className='btn-add' onClick={handleAddBtn}>Add</button>
+        </div>
+
+        <div className='chip-container' >
+          {emailList.map((e, l) => (
+            <div key={l} className='chip' style={{ backgroundColor: e.isValid ? 'lightgreen' : 'lightcoral' }}>
+              <p className='chip-email'>{e.email}</p>
+              <img className="chip-img" alt='avatar' src={getAvatar(e.email)} />
+              <button className="delete-btn" onClick={() => handleDelete(l)}>X</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
